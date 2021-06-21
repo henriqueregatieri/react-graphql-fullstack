c840e3944f4f1b5add4aefed107f81a0ad11cd22
Henrique Regatieri <riq.regatieri@gmail.com>
2021-06-21 17:26:45 -0300

    chore: adds typescript & @apollo/client; removes apollo-booster

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 const client = new ApolloClient({
+  cache: new InMemoryCache(),
   uri: 'http://localhost:4000/graphql',
 });
 
